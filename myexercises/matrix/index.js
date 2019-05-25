cfbad3578d38e4e763be83c9f237fa79216ad897
Raghib Shahriyer <vcshahriyer@gmail.com>
2019-05-25 16:46:38 +0600

    My Solution

--- myexercises/matrix/index.js
+++ myexercises/matrix/index.js
@@ -14,7 +14,40 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+//  matrix(5)
+//     [[1,  2,  3, 4,  5],
+//     [16, 17, 18, 19, 6],
+//     [15, 24, 25, 20, 7],
+//     [14, 23, 22, 21, 8],
+//     [13, 12, 11, 10, 9]]
 
-function matrix(n) {}
+function matrix(n) {
+  let startRow = 0,
+    startCl = 0,
+    endRow = n - 1,
+    endCl = n - 1,
+    count = 1,
+    result = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+  while (startCl <= endCl && startRow <= endRow) {
+    for (let i = startCl; i <= endCl; i++) {
+      result[startRow][i] = count++;
+    }
+    startRow++;
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endCl] = count++;
+    }
+    endCl--;
+    for (let i = endCl; i >= startCl; i--) {
+      result[endRow][i] = count++;
+    }
+    endRow--;
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startCl] = count++;
+    }
+    startCl++;
+  }
+  return result;
+}
 
 module.exports = matrix;
--- myexercises/vowels/index.js
+++ myexercises/vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matcharr = str.match(/[aeiou]/gi);
+  return matcharr ? matcharr.length : 0;
+}
 
 module.exports = vowels;
